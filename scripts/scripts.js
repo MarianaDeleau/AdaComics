1c5ac43e9109ffb482495ad1edc1cd2778395051
Mariana Deleau <marianadeleau@gmail.com>
2021-08-31 16:50:59 -0300

    fetch characters from api

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,7 +9,7 @@ fetch(urlComic)
 })
     .then(function (rta) {
     var comics = rta.data.results;
-    console.log(comics);
+    //console.log(comics)
     var table = document.getElementById('movies');
     var tbody = table.getElementsByTagName('tbody')[0];
     comics.forEach(function (comic) {
@@ -38,6 +38,40 @@ fetch(urlComic)
         td.appendChild(text);
         td2.appendChild(text2);
         td3.appendChild(cover);
+        td4.appendChild(text4);
+        tbody.appendChild(tr);
+    });
+});
+var urlCharacter = BASE_URL + "/characters?ts=1&apikey=" + API_KEY + "&hash=" + HASH;
+fetch(urlCharacter)
+    .then(function (response) {
+    return response.json();
+})
+    .then(function (rta) {
+    var characters = rta.data.results;
+    //console.log(characters)
+    var table = document.getElementById('character');
+    var tbody = document.getElementById('character__table');
+    table.appendChild(tbody);
+    characters.forEach(function (character) {
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+        var td2 = document.createElement('td');
+        var td3 = document.createElement('td');
+        var td4 = document.createElement('td');
+        var text = document.createTextNode(character.name);
+        var text2 = document.createTextNode(character.id);
+        var text4 = document.createTextNode(character.description);
+        var picture = document.createElement('img');
+        picture.setAttribute('src', character.thumbnail.path + "." + character.thumbnail.extension);
+        picture.style.width = '50px';
+        tr.appendChild(td);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        td.appendChild(text);
+        td2.appendChild(text2);
+        td3.appendChild(picture);
         td4.appendChild(text4);
         tbody.appendChild(tr);
     });
